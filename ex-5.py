7f7236c663d3641a73dbee0c17156e7e74a1838f
LameeAlharkosh <jokerasamt2020@gmail.com>
2025-11-11 21:53:54 +0300

    5

--- ex-5.py
+++ ex-5.py
@@ -1,1 +1,20 @@
+import random
 
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def random_100_digit_prime():
+    while True:
+        n = random.randint(10**99, 10**100 - 1)
+        if n % 2 == 0:
+            n += 1
+        if is_prime(n):
+            return n
+
+for i in range(1, 11):
+    print(f"{i}. {random_100_digit_prime()}\n")
