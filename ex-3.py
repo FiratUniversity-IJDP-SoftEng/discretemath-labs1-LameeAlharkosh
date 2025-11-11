a6c513c393eb91fd05a45685133946dbd5568f71
LameeAlharkosh <jokerasamt2020@gmail.com>
2025-11-11 21:53:30 +0300

    3

--- ex-3.py
+++ ex-3.py
@@ -1,1 +1,16 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+for p in range(2, 101):
+    if is_prime(p):
+        mersenne = 2**p - 1
+        if is_prime(mersenne):
+            print(f"2^{p} - 1 = {mersenne} is prime")
+        else:
+            print(f"2^{p} - 1 = {mersenne} is not prime")
 
