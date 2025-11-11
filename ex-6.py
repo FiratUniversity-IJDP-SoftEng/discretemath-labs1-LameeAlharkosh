80f1d6ddaf8e1c60df0b17dbd5b4aa2cc680d181
LameeAlharkosh <jokerasamt2020@gmail.com>
2025-11-11 21:54:05 +0300

    6

--- ex-6.py
+++ ex-6.py
@@ -1,1 +1,19 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+pseudoprimes = []
+
+for n in range(3, 10001):
+    if not is_prime(n):  # نتحقق أنه عدد مركب
+        if pow(2, n - 1, n) == 1:
+            pseudoprimes.append(n)
+
+print("Pseudoprimes to base 2 (n ≤ 10000):")
+print(pseudoprimes)
+print("Count:", len(pseudoprimes))
 
