b09da6e3a7325fadb5ee5c717318f8f173b51f13
LameeAlharkosh <jokerasamt2020@gmail.com>
2025-11-11 21:52:58 +0300

    2

--- ex-2.py
+++ ex-2.py
@@ -1,1 +1,13 @@
+def list_primes(n):
+    for num in range(2, n + 1):
+        is_prime = True
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            print(num, end=" ")
+
+num = int(input("Enter a positive number: "))
+list_primes(num)
 
